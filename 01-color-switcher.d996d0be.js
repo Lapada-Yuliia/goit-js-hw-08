a3ab05b207d0c34236866b76470aca3494c00833
Lapada-Yuliia <128313813+Lapada-Yuliia@users.noreply.github.com>
2024-01-21 20:05:24 +0000

    Deploying to gh-pages from @ Lapada-Yuliia/goit-js-hw-08@eeea02d10bd49451bcc6b9e8cf99a7a959de1671 🚀

---
+++ 01-color-switcher.d996d0be.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),a=document.querySelector("body");t.addEventListener("click",(function(t){t.target.disabled=!0,e.disabled=!1,d=setInterval((()=>{let t=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`;a.style.backgroundColor=t}),1e3)})),e.addEventListener("click",(function(e){e.target.disabled=!0,t.disabled=!1,clearInterval(d),a.style.backgroundColor=""})),e.disabled=!0;let d=null;
+//# sourceMappingURL=01-color-switcher.d996d0be.js.map
